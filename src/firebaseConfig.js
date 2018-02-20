4cc5535aae7200230b6fd56a0375a978cf7bf5d9
Morten Beuchert <mortenbeuchert@gmail.com>
2018-02-20 22:38:02 +0100

    Added firebase config file

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAILr1sXfJ-VFUqTTbC5EOBDBMqT9_kTeM",
+    authDomain: "creative-cards-app-5f75a.firebaseapp.com",
+    databaseURL: "https://creative-cards-app-5f75a.firebaseio.com",
+    projectId: "creative-cards-app-5f75a",
+    storageBucket: "",
+    messagingSenderId: "942855677339"
+  };
+  firebase.initializeApp(config);
