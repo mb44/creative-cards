5a0062541b0b11f2113653ea76687556d19d6460
Morten Beuchert <mortenbeuchert@gmail.com>
2018-02-24 21:49:46 +0100

    Made changes to config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,11 +2,11 @@ import Firebase from 'firebase'
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyAILr1sXfJ-VFUqTTbC5EOBDBMqT9_kTeM",
-    authDomain: "creative-cards-app-5f75a.firebaseapp.com",
-    databaseURL: "https://creative-cards-app-5f75a.firebaseio.com",
-    projectId: "creative-cards-app-5f75a",
-    storageBucket: "",
-    messagingSenderId: "942855677339"
+  apiKey: "AIzaSyAILr1sXfJ-VFUqTTbC5EOBDBMqT9_kTeM",
+  authDomain: "creative-cards-app-5f75a.firebaseapp.com",
+  databaseURL: "https://creative-cards-app-5f75a.firebaseio.com",
+  projectId: "creative-cards-app-5f75a",
+  storageBucket: "creative-cards-app-5f75a.appspot.com",
+  messagingSenderId: "942855677339"
   };
   Firebase.initializeApp(config);
