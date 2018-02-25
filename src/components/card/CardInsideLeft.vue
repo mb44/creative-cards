52a1bcdd367b51b339b6420727ec436bccf97f0b
Morten Beuchert <mortenbeuchert@gmail.com>
2018-02-25 13:08:26 +0100

    Add TextInput and TextInput components

--- src/components/card/CardInsideLeft.vue
+++ src/components/card/CardInsideLeft.vue
@@ -1,15 +1,33 @@
 <template>
     <div class="row">
         <div class="col-sm-6 card edit-area">
-            <p>cardInsideLeft</p>
+            <cc-text-input @displayTextChanged="textBoxValue1 = $event"></cc-text-input>
         </div>
-        <div class="col-sm-6 card card-display"></div>
+        <div class="col-sm-6 card card-display">
+             <cc-text-output :displayText="textBoxValue1" :containerHeight="750"></cc-text-output>
+        </div>
     </div>
 </template>
 
 <script>
+import TextInput from './TextInput.vue'
+import TextOutput from './TextOutput.vue'
+
+export default {
+    data: function() {
+        return {
+            textBoxValue1: '',
+            imageName: ''
+        }
+    },
+    components: {
+        ccTextInput: TextInput,
+        ccTextOutput: TextOutput,
+    }
+}
 
 </script>
+
 
 <style>
 </style>
