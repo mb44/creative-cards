90e71cfcd854ce3fc88bf45f4304986ee46023d4
Morten Beuchert <mortenbeuchert@gmail.com>
2018-02-25 13:16:09 +0100

    Add ImageInput and ImageOutput components

--- src/components/card/CardBack.vue
+++ src/components/card/CardBack.vue
@@ -1,13 +1,36 @@
 <template>
     <div class="row">
         <div class="col-sm-6 card edit-area">
-            <p>cardBack</p>
+            <cc-image-upload @displayImageChanged="imageName = $event"></cc-image-upload><hr>
         </div>
-        <div class="col-sm-6 card card-display"></div>
+        <div class="col-sm-6 card card-display">
+            <cc-image-output :displayImage="imageName" :containerHeight="400" :clearImageProp="clearImage"></cc-image-output>
+        </div>
     </div>
 </template>
 
 <script>
+import ImageUpload from './ImageUpload.vue'
+import ImageOutput from './ImageOutput.vue'
+
+export default {
+    data: function() {
+        return {
+            imageName: ''
+        }
+    },
+    methods: {
+        clearImage: function() {
+            if (this.imageName != '') {
+                this.imageName = "beach.jpg"
+            }
+        }
+    },
+    components: {
+        ccImageUpload: ImageUpload,
+        ccImageOutput: ImageOutput
+    }
+}
 
 </script>
 
