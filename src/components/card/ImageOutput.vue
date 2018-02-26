dd4acc68e6b616d64d791f9b1928c0d2e222a750
Morten Beuchert <mortenbeuchert@gmail.com>
2018-02-26 21:24:08 +0100

    Change

--- src/components/card/ImageOutput.vue
+++ src/components/card/ImageOutput.vue
@@ -72,23 +72,4 @@
     img {
         width: 130%;
     }
-
-    .scale-enter-active {
-        animation: scale-in 0.5s;
-    }
-
-    .scale-leave-active {
-        animation: scale-out 0.5s;
-    }
-
-    @keyframes scale-in {
-    0% { transform: scale(0); }
-    100% { transform: scale(1); }
-    }
-
-    @keyframes scale-out {
-    0% { transform: scale(1); }
-    100% { transform: scale(0); }
-    }
-
 </style>
